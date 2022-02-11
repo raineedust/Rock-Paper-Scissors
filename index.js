8863765b8a56221c4d5a58e9a8a12abc81c5cd3a
raineedust <rayneedust@gmail.com>
2022-02-12 05:13:29 +0800

    Add loop for 5 rounds, add console feedback

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-//a bunch of constants
-let item = ["rock", "paper", "scissors"];
-let computerScore = 0
-let playerScore = 0
+//Define item array
+const item = ["rock", "paper", "scissors"];
 
-// Computer randomly select r/p/s
+// Computer randomly select r/p/s from item array
 
 function computerPlay() {
     let choice = item[Math.floor(Math.random() * item.length)];
     return choice;
 }
 
-// Prompt for player choice and initiate first round
+// Prompt for player choice, determine win/loss/invalid choice, output result
 
 let playerSelection;
 let computerSelection;
+let i;
 
 function playRound (playerSelection, computerSelection) {
     playerSelection=prompt("Rock, Paper, or Scissors?").toLowerCase();
     computerSelection=computerPlay();
-    console.log(`You chose ${playerSelection}.`)
-    console.log(`The computer chose ${computerSelection}.`)
 
     if (playerSelection===computerSelection) {
-        let result = ("It's a tie!");
+        let result = ("It's a tie round at " + playerScore + "-" + computerScore + "!");
         console.log(result)
         return result;
     } 
@@ -32,7 +29,7 @@ function playRound (playerSelection, computerSelection) {
         (playerSelection==="paper" && computerSelection==="rock") ||
         (playerSelection==="scissors" && computerSelection==="paper")) {
             playerScore++;
-            let result = ("You won the round! " + playerSelection + " beats " + computerSelection);
+            let result = ("You won the round at " + playerScore + "-" + computerScore + "! " + playerSelection + " beats " + computerSelection + ".");
             console.log(result)
             return result;
     } 
@@ -42,23 +39,51 @@ function playRound (playerSelection, computerSelection) {
         (playerSelection==="paper" && computerSelection==="scissors") ||
         (playerSelection==="scissors" && computerSelection==="rock")) {
             computerScore++;
-            let result = ("You won the round! " + computerSelection + " beats " + playerSelection);
+            let result = ("You lost the round at " + playerScore + "-" + computerScore + ". " + computerSelection + " beats " + playerSelection + ".");
             console.log(result)
             return result;
         }
         
-        else {
+    else if (playerSelection != "rock" || "paper" || "scissors") {
         let result = "Invalid choice. Please choose between Rock, Paper, or Scissors.";
+        console.log(result)
         return result;
     }
 }
 
-// Play for five rounds
+// Define scores, loop for 5 rounds
+
+let computerScore = 0
+let playerScore = 0
+
+game(playerSelection);
 
 function game(playerSelection) {
     for (let i = 0; i < 5; i++) {
-        playRound(playerSelection)
+        playRound(playerSelection);
+    }
+    // Output final result
+    if (playerScore >=3) {
+        finalResult();
+    }
+
+    else if (computerScore >= 3) {
+        finalResult();
+    }
+
+    else if (i = 5) {
+    finalResult();
     }
 }
 
-game(playerSelection);
+// Final result
+
+function finalResult() {
+    if (playerScore > computerScore) {
+        console.log("Congrats, you've won the match!")
+    }
+
+    else if (playerScore < computerScore) {
+        console.log("You lost the match. Better luck next time!")
+    }
+}
